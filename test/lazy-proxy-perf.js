528ae6e2f55c0193d8fcf657294aafb9ef1dc416
John Bender <john.m.bender@gmail.com>
2012-04-10 20:24:23 -0700

    add test file for jsperf, create test subdir

---
+++ test/lazy-proxy-perf.js
@@ -0,0 +1,35 @@
+for( var i = 0; i <= 100; i++ ){
+  var div = document.createElement("div");
+  div.setAttribute("data-test", "true");
+  $( div ).appendTo( "body" );
+}
+
+var addClassRaw = function( elem ){
+  elem.setAttribute( "class", elem.getAttribute( "class" + " foo" ));
+  return elem;
+};
+
+var clearClassRaw = function( elem ) {
+  elem.setAttribute( "class", "" );
+  return elem;
+};
+
+$.fn.simpleAddClass = function(){
+  $.map(this, addClassRaw);
+  return this;
+};
+
+$.fn.simpleClearClass = function(){
+  $.map(this, clearClassRaw);
+  return this;
+};
+
+$.fn.lazyClearClass = $.functor(clearClassRaw);
+
+$.fn.lazyAddClass = $.functor(addClassRaw);
+
+$.fn.manualAddClearClass = function(){
+  $.map(this, function( elem ){
+    addClassRaw(clearClassRaw(elem));
+  });
+};
